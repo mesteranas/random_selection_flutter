0910b2911e4cdeb81134a111792613b9ede0be99
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-06-29 13:32:53 +0300

    29/06/2024

---
+++ random_selection/android/app/src/main/kotlin/com/example/random_selection/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.random_selection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ random_selection/lib/app.dart
@@ -0,0 +1,7 @@
+class App{
+  static const name="random selectionanas main";
+  static const appName="random_selection_flutter";
+  static const appDirName="random_selection";
+  static const version=1.0;
+  static const description="a app makes you to choose random names";
+}
